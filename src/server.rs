0e013cf6827703fbe0cff636187aaee0b637f587
Dario Ostuni <dario.ostuni@gmail.com>
2021-03-21 18:50:04 +0100

    Add systemd-journald support

--- src/server.rs
+++ src/server.rs
@@ -10,6 +10,9 @@ mod tuning;
 use clap::Clap;
 use tokio::runtime::Runtime;
 use tracing::error;
+use tracing_subscriber::{
+    filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt, Registry,
+};
 
 #[derive(Clap, Debug)]
 struct CliArgs {
@@ -19,14 +22,30 @@ struct CliArgs {
     listen_port: u16,
     #[clap(short, long, about = "Verification password")]
     verification_password: Option<String>,
+    #[clap(short, long, about = "Send logs to journald")]
+    journald: bool,
     #[cfg(unix)]
     #[clap(short, long, about = "Use bind address as a Unix Domain Socket")]
     unix_domain_socket: bool,
 }
 
 fn main() {
-    tracing_subscriber::fmt::init();
     let args = CliArgs::parse();
+    if args.journald {
+        let layer = match tracing_journald::layer() {
+            Ok(x) => x,
+            Err(x) => {
+                println!("Cannot initialize journald logger: {}", x);
+                return;
+            }
+        };
+        Registry::default()
+            .with(EnvFilter::from_default_env())
+            .with(layer)
+            .init();
+    } else {
+        tracing_subscriber::fmt::init();
+    }
     match Runtime::new() {
         Ok(rt) => rt.block_on(async move { master::start(args).await }),
         Err(x) => error!("Cannot create tokio runtime: {}", x),
